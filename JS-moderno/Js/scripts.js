2d220b985c8524060d70c21ac8566d0fa9da15cc
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-08-06 18:02:01 -0300

    finalizei de acrecentar mais coisas em meu html e scripts testei e funcionou.

--- JS-moderno/Js/scripts.js
+++ JS-moderno/Js/scripts.js
@@ -1,42 +1,89 @@
-// var, let  e const
-var x=10;
-var y=15;
+// 1 - var, let e const
+var x = 10;
+var y = 15;
 
-if (y >10){
-    var x =5;
-    console.log(x);
+if (y > 10) {
+    var x = 5;
+    console.log(x); 
 }
 
-console.log(x);
+console.log(x); 
 
-let a =10;
-let b =15;
+let a = 10;
+let b = 15;
 
-if (b >10){
-    let a =5;
+if (b > 10) {
+    let a = 5;
     console.log(a);
 }
 
-console.log(a);
+console.log(a); 
 
-function logName(){
+function logName() {
     const name = "Gabriel";
-    console.log(name);
+    console.log(name); 
 }
 
-const name ="Beatriz";
+const name = "Beatriz";
 
 logName();
 
-console.log(name);
+console.log(name); 
 
-//  arroy  function //
-const sum =function (a,b){
-    return a+ b
-}
+// 2 - Arrow function
+const sum = function (a, b) {
+    return a + b;
+};
 
-const arroySum= (a, b) => a+ b;
+const arrowSum = (a, b) => a + b;
 
-console.log(sum (5 ,5));
+console.log(sum(5, 5)); 
+console.log(arrowSum(5, 5)); 
 
-console.log(arroySum(5 ,5));
+const greeting = (name) => {
+    if (name) {
+        return `Hello ${name}!`;
+    } else {
+        return "Hello";
+    }
+};
+
+console.log(greeting("Beatriz")); 
+console.log(greeting());
+const user = {
+    name: "Gabriel",
+    sayUserName() {
+        setTimeout(function () {
+            console.log(this); 
+            console.log(`Username: ${this.name}`);
+        }, 1000);
+    },
+    sayUserNameArrow() {
+        setTimeout(() => {
+            console.log(this); 
+            console.log(`Username: ${this.name}`); 
+        }, 2000);
+    },
+};
+
+// user.sayUserName();
+// user.sayUserNameArrow();
+
+// 3 - filter
+const arr = [1, 2, 3, 4, 5, 6];
+
+const highNumbers = arr.filter((n) => n >= 3);
+
+console.log(highNumbers); 
+
+const users = [
+    { name: "Beatriz", available: true },
+    { name: "Ana Paula", available: false },
+    { name: "Simone", available: true },
+    { name: "Priscila", available: false },
+    { name: "Dacir", available: true },
+];
+
+const availableUsers = users.filter((user) => user.available);
+
+console.log(availableUsers); 
