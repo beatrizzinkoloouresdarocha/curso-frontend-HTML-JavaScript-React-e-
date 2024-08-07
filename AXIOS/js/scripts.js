c541a279d09e3dda0cf41c8e517eddacc309cb93
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-08-07 17:25:06 -0300

    acabei de criar global.js .

--- AXIOS/js/scripts.js
+++ AXIOS/js/scripts.js
@@ -52,3 +52,18 @@ const printData = async () => {
 };
 
 printData();
+
+//5- post //
+const form =document.querySelector("#post-form")
+const titleInput =document.querySelector("#title")
+const bodyInput =document.querySelector("#body")
+
+form .addEventListener("submit", (e)=>{
+    e.preventDefault();
+
+    axios.post("https://jsonplaceholder.typicode.com/users",{
+        title: titleInput.value,
+        body:bodyInput.value,
+        userID:1,
+    });
+});
