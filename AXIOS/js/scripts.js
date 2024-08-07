41b726250536fade42527a8a5420ab4bb10fccb8
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-08-07 15:25:48 -0300

    estou acrecentando mais coisas em meu html e no meu scripts.js.

--- AXIOS/js/scripts.js
+++ AXIOS/js/scripts.js
@@ -18,3 +18,23 @@ const getData = async() =>{
 };
 
 getData()
+
+// 3-imprimindo dados na tela //
+const container = document.querySelector("#user-container");
+
+const printData = async () => {
+    const data = await getData();
+    console.log(data);
+
+    data.forEach((user) => {
+        const div = document.createElement("div");
+
+        const nameElement = document.createElement("h2");
+        nameElement.textContent = user.name;
+
+        div.appendChild(nameElement);
+        container.appendChild(div);
+    });
+};
+
+printData();
